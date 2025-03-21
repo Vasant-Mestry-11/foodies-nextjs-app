868d4074a0282969332045e6912afced421b3c1f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-03-21 21:13:41 +0530

    :zap: :art: main header background

--- components/main-header.js
+++ components/main-header.js
@@ -2,23 +2,27 @@ import LogoImg from '@/assets/logo.png'
 import Link from "next/link";
 import classes from './main-header.module.css'
 import Image from 'next/image';
+import MainHeaderBackground from './main-header-background';
 
 export default function MainHeader() {
 
-  return <header className={classes.header}>
+  return <>
+    <MainHeaderBackground />
+    <header className={classes.header}>
 
-    {/* logo */}
-    <Link href="/" className={classes.logo}>
-      <Image src={LogoImg} alt='logo' priority />
-      NextLevel Food
-    </Link>
+      {/* logo */}
+      <Link href="/" className={classes.logo}>
+        <Image src={LogoImg} alt='logo' priority />
+        NextLevel Food
+      </Link>
 
-    {/* navigation */}
-    <nav className={classes.nav}>
-      <ul>
-        <li><Link href="/meals">Browse Meals</Link></li>
-        <li><Link href="/community">Foodie Community</Link></li>
-      </ul>
-    </nav>
-  </header>
+      {/* navigation */}
+      <nav className={classes.nav}>
+        <ul>
+          <li><Link href="/meals">Browse Meals</Link></li>
+          <li><Link href="/community">Foodie Community</Link></li>
+        </ul>
+      </nav>
+    </header>
+  </>
 }
