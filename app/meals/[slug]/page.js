5e2e3daea03fa349c412fdbb2aa800de2fbdcf40
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-03-29 13:28:19 +0530

    :zap: :art: Meal Page with animations

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,6 +1,27 @@
+import Image from 'next/image'
+import classes from './page.module.css'
+import { getMeal } from '@/lib/meals'
+
 export default function MealsDetailPage({ params }) {
-  return <main>
-    <h1>Meals Detail Component</h1>
-    <p>{params.slug}</p>
-  </main>
+  const meal = getMeal(params.slug)
+  const { title, summary, instructions, creator_email, creator, image } = meal;
+  return <>
+    <header className={classes.header}>
+      <div className={classes.image}>
+        <Image src={image} alt={title} fill />
+      </div>
+      <div className={classes.headerText}>
+        <h1>{title}</h1>
+        <p className={classes.creator}>
+          by <a href={`mailto:${creator_email}`}>{creator}</a>
+        </p>
+        <p className={classes.summary}>{summary}</p>
+      </div>
+    </header>
+    <main>
+      <p className={classes.instructions} dangerouslySetInnerHTML={{
+        __html: instructions
+      }}></p>
+    </main>
+  </>
 }
